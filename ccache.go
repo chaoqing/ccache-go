b4a6b941d791e77ec51d764ba578e23fdb6e0e01
Nick Wang <chaoqingwang.nick@gmail.com>
2020-08-12 14:52:06 +0800

    auto skip icpc AVX options;

--- ccache.go
+++ ccache.go
@@ -13,6 +13,8 @@ import "C"
 import (
 	log "github.com/sirupsen/logrus"
 	"os"
+	"path/filepath"
+	"regexp"
 	"unsafe"
 )
 
@@ -36,8 +38,92 @@ func cMain(args []string){
 	C.ccache_main(argc, (**C.char)(unsafe.Pointer(argv)))
 }
 
+
+type PatternSet struct {
+	Values       []string
+	IsRegex      bool
+	regexPattern []*regexp.Regexp
+}
+
+func (t *PatternSet) compile() {
+	if t.IsRegex{
+		for _, s := range t.Values {
+			t.regexPattern = append(t.regexPattern, regexp.MustCompile(s))
+		}
+	}
+}
+
+func (t *PatternSet) match(v string) bool{
+	if t.IsRegex{
+		for _, p := range t.regexPattern{
+			if p.MatchString(v) {
+				return true
+			}
+		}
+		return false
+	}else{
+		for _, p := range t.Values{
+			if p==v {
+				return true
+			}
+		}
+		return false
+	}
+}
+
+type Rule struct {
+	command *PatternSet
+	arg *PatternSet
+}
+
+func(r *Rule) match(command, arg string) bool{
+	return r.arg.match(arg) && (len(r.command.Values)==0 || r.command.match(command))
+}
+
+const (
+	extraOption = "--ccache-skip"
+)
+
+var(
+	rules = []*Rule{
+		&Rule{
+			command: &PatternSet{Values: []string{"icp?c"}, IsRegex: true},
+			arg: &PatternSet{Values: []string{"-xAVX", "-xCORE_AVX2"}},
+			},
+	}
+)
+
+func PrepareArgs(origArgs []string) []string {
+	args := make([]string, 0, len(origArgs)*2)
+	args = append(args, origArgs[0])
+
+	command := filepath.Base(origArgs[0])
+	for _, arg := range origArgs[1:]{
+		for _, r := range rules{
+			if r.match(command, arg){
+				args = append(args, extraOption)
+				break
+			}
+		}
+		args = append(args, arg)
+	}
+	return args
+}
+
+func init(){
+	for _, r := range rules{
+		r.command.compile()
+		r.arg.compile()
+	}
+}
+
+
 func main(){
-	args:=os.Args
+	args := PrepareArgs(os.Args)
+	log.Debugln(args)
 
 	cMain(args)
 }
+
+
+
